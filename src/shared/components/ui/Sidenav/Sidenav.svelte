64b1b20f3fc643bd81eef5006cbf7d2b594927ec
carloscabral656 <edu.carlos.cec@gmail.com>
2026-03-29 15:40:21 -0300

    sidenav

--- src/shared/components/ui/Sidenav/Sidenav.svelte
+++ src/shared/components/ui/Sidenav/Sidenav.svelte
@@ -5,8 +5,11 @@
   let { items }: { items: SidenavItemProps[] } = $props();
 </script>
 
-<nav class="h-svh w-[200px] bg-[#061326]">
-  <ul class="flex flex-col gap-2 p-2 text-white">
+<nav class="h-svh w-[200px] bg-[#061326] py-4">
+  <h3 class="px-4 font-bold text-[#E2E8F0]">Sprint 24</h3>
+  <h4 class="px-4 text-sm text-[#64748B]">Active Session</h4>
+
+  <ul class="mt-4 flex flex-col gap-2 p-2 text-white">
     {#each items as item (item.id)}
       <SidenavItem {item} />
     {/each}
