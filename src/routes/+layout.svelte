29be23f54f477e1f61f23ea1bc52e192e8e83b3d
carloscabral656 <edu.carlos.cec@gmail.com>
2026-03-29 19:18:16 -0300

    moving to layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,51 @@
 <script lang="ts">
   import './layout.css';
-  import favicon from '$lib/assets/favicon.svg';
+  import { ListCheck, MessageCircle, Radio, Users } from '@lucide/svelte';
+  import { v4 as uuidv4 } from 'uuid';
+  import Header from '../shared/components/ui/Header/Header.svelte';
+  import Sidenav from '../shared/components/ui/Sidenav/Sidenav.svelte';
+  import type { SidenavItemProps } from '../shared/components/ui/Sidenav/SidenavItem.props';
 
   let { children } = $props();
+
+  const items: SidenavItemProps[] = [
+    {
+      id: uuidv4(),
+      name: 'Live Feed',
+      icon: Radio,
+      active: true,
+    },
+    {
+      id: uuidv4(),
+      name: 'Live Feed',
+      icon: Users,
+      active: true,
+    },
+    {
+      id: uuidv4(),
+      name: 'Story Queue',
+      icon: ListCheck,
+      active: true,
+    },
+    {
+      id: uuidv4(),
+      name: 'Story Queue',
+      icon: MessageCircle,
+      active: true,
+    },
+  ];
 </script>
 
-<svelte:head><link rel="ic on" href={favicon} /></svelte:head>
-{@render children()}
+<div class="min-h-screen">
+  <Header />
+
+  <div class="grid grid-cols-12">
+    <aside class="col-span-2">
+      <Sidenav {items} />
+    </aside>
+
+    <main class="col-span-10 bg-[#040e1f] p-4 text-[#E2E8F0]">
+      {@render children()}
+    </main>
+  </div>
+</div>
